ce9cd32bab6872649eb07617769cba0170ed0a97
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-09-26 13:06:31 +0530

    C syntax

---
+++ c_syntax.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include <stdio.h> is a header file library that lets us work with input and output functions, such as printf()
+//Header files add functionality to C programs
+
+//main() is a function. Any code inside its curly brackets {} will be executed
+int main() {
+
+    //printf() is a function used to output/print text to the screen
+    printf("Hello World!");
+    //return 0 ends the main() function
+    return 0;
+
+    //Every C statement ends with a semicolon 
+
+}
