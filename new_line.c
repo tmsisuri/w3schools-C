be81ba61452a7949b1d32298e28155715ea7e7e1
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-10-15 09:16:04 +0530

    get a blank line in c

--- new_line.c
+++ new_line.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 int main() {
     //you can go to a new line by using the newline character (\n)
-    
+
+    ////after printing "Hello, World!", the \n moves the cursor to the next line, so the next printf() starts its output on a new line
+    //You can use \n anywhere in the string to create line breaks
     printf("Hello World!\n");
-    printf("This is a new line\n");
+    printf("This is a new line\n\n");
+    
+    //Two \n characters after each other will create a blank line
+
+    //You can also output multiple lines with a single printf() function
+    printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+
     return 0;
 
-    //after printing "Hello, World!", the \n moves the cursor to the next line, so the next printf() starts its output on a new line
-    //You can use \n anywhere in the string to create line breaks
 }
