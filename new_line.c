55c312368540f444dcbaf8d9c6596438be022498
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-10-15 08:59:49 +0530

    how to go to the new line in c

---
+++ new_line.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    //you can go to a new line by using the newline character (\n)
+    
+    printf("Hello World!\n");
+    printf("This is a new line\n");
+    return 0;
+
+    //after printing "Hello, World!", the \n moves the cursor to the next line, so the next printf() starts its output on a new line
+    //You can use \n anywhere in the string to create line breaks
+}
