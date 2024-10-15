89eb2e4f5cf3c8318309c61bab7e26fc10f092c0
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-10-15 08:50:34 +0530

    c statements

---
+++ c_statements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    //A computer program is a list of "instructions" to be "executed" by a computer
+    //In a programming language, these programming instructions are called statements
+    //The following statement "instructs" the compiler to print the text "Hello World" to the screen
+
+    printf("Hello World ");
+    printf("Have a good day!");
+    return 0;
+
+    //The statements are executed, one by one, in the same order as they are written
+    //The first statement is executed first (print "Hello World!" to the screen)
+    //Then the second statement is executed (print "Have a good day!" to the screen)
+    //And at last, the third statement is executed (end the C program successfully)
+}
