1633a0bd24fa30d137d6d7c01d8cd30e39843983
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-10-21 20:57:57 +0530

    variables in c

---
+++ variables.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    //Variables are containers for storing data values, like numbers and characters
+    //In C, there are different types of variables (defined with different keywords)
+
+    //int - stores integers (whole numbers), without decimals, such as 123 or -123
+    //float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+    //char - stores single characters, such as 'a' or 'B'. Characters are surrounded by single quotes
+
+    //To create a variable, specify the type and assign it a value
+
+    int num = 15;
+    float f = 34.56;
+    char c = 'k';
+
+    //You can also declare a variable without assigning the value, and assign the value later
+
+    int num1;  // Declare a variable
+    num1 = 12;  // Assign a value to the variable
+    
+    return 0;
+}
