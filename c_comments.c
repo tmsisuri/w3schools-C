35eca21d124ae9d9b10a379ec556a2d172f368d3
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-10-21 20:36:12 +0530

    comments in c

---
+++ c_comments.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    //Comments can be used to explain code, and to make it more readable 
+    //It can also be used to prevent execution when testing alternative code
+    //Comments can be singled-lined or multi-lined
+
+    //Single-line Comments
+
+    //Single-line comments start with two forward slashes (//)
+    //Any text between // and the end of the line is ignored by the compiler (will not be executed)
+
+    //Multi-line Comments
+    //Multi-line comments start with /* and ends with */
+    //Any text between /* and */ will be ignored by the compiler
+
+    return 0;
+}
