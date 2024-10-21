7b398a671a54e9bdc04639b5a49efe8f75834c5c
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-10-21 22:08:14 +0530

    calculate the area of a rectangle

---
+++ area_of_a_rectangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    // Create integer variables
+    int length = 4;
+    int width = 5;
+    int area;
+
+    // Calculate the area of a rectangle
+    area = length * width;
+
+    // Print the variables
+    printf("Length is : %d\n", length);
+    printf("Width is : %d\n", width);
+    printf("Area of the rectangle is : %d", area);
+
+    return 0;
+}
